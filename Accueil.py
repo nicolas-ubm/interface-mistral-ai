2ab430a676e53e35ee332f7dafacab8e338786e2
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 11:01:14 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -3,23 +3,14 @@ import streamlit as st
 # Application sans doute non fonctionnelle (tests dans le cadre d'une formation)
 st.title('Interface Mistral AI')
 
-# Sous-titre
-st.subheader('Mistral AI')
+# ====== Sidebar ======
+# Demande clé API
+mistral_api_key = st.text_input("Quel est votre nom ?")
+st.text_input("Entrez votre clé API", type="password", key="api_key", help="Générez une clé sur le site https://mistral.ai/")
 
 # Zones de texte
 st.write("Introduction à Mistral AI")
 
-
-if st.checkbox("Afficher l'aide de mise en forme du contenu"):
-  st.write("""
-  # Titre
-  
-  ## Soustitre
-  
-  **Gras**
-  
-  `print("Hello World!)`
-  """)
 
 # Zone de saisie
 user_name = st.text_input("Quel est votre nom ?")
@@ -61,6 +52,3 @@ st.download_button(
 )
 
 
-# ====== Sidebar ======
-# Image dans la sidebar
-st.sidebar.image("https://mistral.ai/images/logo_hubc88c4ece131b91c7cb753f40e9e1cc5_2589_256x0_resize_q97_h2_lanczos_3.webp")
