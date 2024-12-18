0ae6ed422ff0bb54365f0f0d085ecbb6d34fcdae
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 09:24:47 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -3,4 +3,25 @@ import streamlit as st
 # Application sans doute non fonctionnelle (tests dans le cadre d'une formation)
 st.title('Interface Mistral AI')
 
+# Sous-titre
 st.subheader('Mistral AI')
+
+# Zones de texte
+st.write("Introduction à Mistral AI")
+
+st.write("""
+# Titre
+## Soustitre
+**Gras**
+`print("Hello World!)`
+
+""")
+
+# Bouton
+
+
+# Slider
+
+
+# Lecture d'un fichier CSV avec pandas
+
