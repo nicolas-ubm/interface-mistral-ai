908d527b8e7fa596bb5b09f2565b37f44f52c7a4
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 10:13:10 +0100

    Update Traduction.py

--- pages/Traduction.py
+++ pages/Traduction.py
@@ -1,4 +1,19 @@
 import streamlit as st
-from functions import *
+
 
 st.title('Traduction')
+
+
+import os
+import streamlit as st
+
+# Obtenir le chemin du répertoire courant
+current_directory = os.getcwd()
+
+# Lister tous les fichiers dans le répertoire courant
+files = os.listdir(current_directory)
+
+# Afficher la liste des fichiers
+st.write("Fichiers dans le dossier courant :")
+for file in files:
+    st.write(file)
