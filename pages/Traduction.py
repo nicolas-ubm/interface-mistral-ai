1fc9235493ee55be34565c54b92f227909c3862f
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 11:09:12 +0100

    Update Traduction.py

--- pages/Traduction.py
+++ pages/Traduction.py
@@ -3,9 +3,19 @@ from functions import *
 from mistralai import Mistral
 
 
+# ====== Sidebar ======
+# Demande clé API
+mistral_api_key = st.sidebar.text_input("Entrez votre clé API", type="password", key="api_key", help="Générez une clé sur le site https://mistral.ai/")
+
+if mistral_api_key:
+    st.write("Introduction à Mistral AI")
+else:
+    st.sidebar.warning("Veuillez entrer une clé API valide pour continuer.")
+
+# ====== Contenu ======
 st.title('Traduction')
-API_KEYS = st.sidebar.text_input("Quel est votre clé API Mistral ?")
-client = Mistral(api_key = API_KEYS)
+
+client = Mistral(api_key = mistral_api_key)
 
 prompt = st.text_area("Prompt :")
 
