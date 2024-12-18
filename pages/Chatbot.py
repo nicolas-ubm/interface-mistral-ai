b15d0e883f87a34a514e6ae1b3a1e3ee1c16025c
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 14:21:35 +0100

    Update Chatbot.py

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -1,26 +1,37 @@
 import streamlit as st
+from functions import get_ner
+from mistralai import Mistral
 
-st.title("Echo Bot")
+st.title("Discussion avec Mistral Agent")
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# Demande une clé API
+mistral_api_key = st.sidebar.text_input("Entrez votre clé API", type="password", key="api_key", help="Vous pouvez générer une clé sur le site https://mistral.ai/")
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+if not mistral_api_key:
+    st.sidebar.warning("Veuillez entrer une clé API valide pour continuer.")
+else:
+    client = Mistral(api_key=mistral_api_key)
 
-# React to user input
-if prompt := st.chat_input("Que puis-je faire pour vous ?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    # React to user input
+    if prompt := st.chat_input("Que puis-je faire pour vous ?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+    
+        #response = f"MIA: {prompt}"
+        response = get_ner(client, prompt)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
