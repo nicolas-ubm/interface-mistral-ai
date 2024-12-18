df50ccc8e859c99c28f8de7e32b5d0e46b8d6f4d
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 11:10:03 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -3,4 +3,11 @@ import streamlit as st
 # Application sans doute non fonctionnelle (tests dans le cadre d'une formation)
 st.title('Interface Mistral AI')
 
+# ====== Sidebar ======
+# Demande clé API
+mistral_api_key = st.sidebar.text_input("Entrez votre clé API", type="password", key="api_key", help="Générez une clé sur le site https://mistral.ai/")
 
+if mistral_api_key:
+    st.write("Introduction à Mistral AI")
+else:
+    st.sidebar.warning("Veuillez entrer une clé API valide pour continuer.")
