f9b5b68ade1e7079ac0939b3626c0ecc14f3df01
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 11:25:13 +0100

    Update Traduction.py

--- pages/Traduction.py
+++ pages/Traduction.py
@@ -2,22 +2,20 @@ import streamlit as st
 from functions import *
 from mistralai import Mistral
 
-
-# ====== Sidebar ======
-# Demande clé API
-mistral_api_key = st.sidebar.text_input("Entrez votre clé API", type="password", key="api_key", help="Générez une clé sur le site https://mistral.ai/")
-
-if mistral_api_key:
-    st.write("Introduction à Mistral AI")
-else:
-    st.sidebar.warning("Veuillez entrer une clé API valide pour continuer.")
-
-# ====== Contenu ======
 st.title('Traduction')
 
-client = Mistral(api_key = mistral_api_key)
+# Demande une clé API
+mistral_api_key = st.sidebar.text_input("Entrez votre clé API", type="password", key="api_key", help="Vous pouvez générer une clé sur le site https://mistral.ai/")
 
-prompt = st.text_area("Prompt :")
+if not mistral_api_key:
+    st.sidebar.warning("Veuillez entrer une clé API valide pour continuer.")
+else:
+    client = Mistral(api_key=mistral_api_key)
+    
+    prompt = st.text_area("Entrez le texte à traduire :")
+    trad_lang = st.selectbox("Sélectionnez la langue", ["Français", "Allemand", "Anglais", "Espagnol", "Japonais"], index=2)
+        
+        if st.button("Exécuter") and prompt:
+            response = get_ner(client, prompt)
+            st.write(response)
 
-response = get_ner(client, prompt)
-st.write(response)
