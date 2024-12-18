a589676a9593edc8694a687eab5128bac8a54654
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 15:43:42 +0100

    Update Historique.py

--- pages/Historique.py
+++ pages/Historique.py
@@ -14,15 +14,18 @@ def display_and_edit_csv(file):
         
         # Affichage des données
         st.subheader("Données de l'historique")
-        edited_df = st.experimental_data_editor(df, num_rows="dynamic", key="data_editor")
+        edited_df = st.data_editor(df, num_rows="dynamic", key="data_editor")
         
         # Nom par défaut pour enregistrer le fichier
         default_filename = file.name if hasattr(file, 'name') else "historique_chatbot.csv"
         filename = st.text_input("Nom du fichier pour l'enregistrement", value=default_filename, key="save_filename")
 
         # Bouton pour enregistrer
+        def convert_df(df):
+            return df.to_csv(index=False).encode('utf-8')
+
         if st.button("Enregistrer le fichier"):
-            csv_data = edited_df.to_csv(index=False).encode('utf-8')
+            csv_data = convert_df(edited_df)
             st.download_button(
                 label="Télécharger le fichier modifié",
                 data=csv_data,
