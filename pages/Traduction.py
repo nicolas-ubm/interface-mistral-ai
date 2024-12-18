f8f9e44cc26062adeceb87fae5c06e611815a8a6
Nicolas <nicolas.henriet@u-bordeaux-montaigne.fr>
2024-12-18 10:30:53 +0100

    Update Traduction.py

--- pages/Traduction.py
+++ pages/Traduction.py
@@ -1,4 +1,13 @@
 import streamlit as st
 from functions import *
+from mistralai import Mistral
+
 
 st.title('Traduction')
+API_KEYS = st.sidebar.text_input("Quel est votre clé API Mistral ?")
+client = Mistral(api_key = API_KEYS)
+
+prompt = st.text_area("Prompt :")
+
+response = get_ner(client, prompt)
+st.write(response)
